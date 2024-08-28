bbdd03e72889effeb2816117d42f4c335c0368d4
AlfianFirja <alfian.firja@gmail.com>
2024-08-28 14:34:05 +0700

    samlekom

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%7s422vi*ut90do%e5k_j@)%h4sd*&kptv*ie12(o9sdgglqkp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "alfian-bassam-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
